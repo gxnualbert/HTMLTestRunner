cd410c05f0d0b01ac0078d06a7f74c78e72d08a1
gxnualbert <albert.chenwesoft@gmail.com>
2016-08-27 14:53:14 +0800

    new test

--- UseFirefoxDefaultProfile.py
+++ UseFirefoxDefaultProfile.py
@@ -13,41 +13,29 @@ class Baidu(unittest.TestCase):
 
     def setUp(self):
     
+        appData=os.getenv('APPDATA')
+        profile=appData+"\\Mozilla\\Firefox\\Profiles\\"
+        current=os.listdir(profile)
+        fireFoxProfile=profile+current[0]
+        fp=webdriver.FirefoxProfile(fireFoxProfile)
         
-        profile_dir=r"C:\Users\Administrator\AppData\Local\Google\Chrome\User Data"    # 对应你的chrome的用户数据存放路径  
-        chrome_options=webdriver.ChromeOptions()  
-        chrome_options.add_argument("user-data-dir="+os.path.abspath(profile_dir))  
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)  
+        self.driver = webdriver.Firefox(fp)
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.zhihu.com/"
+        self.base_url = "https://www.baidu.com"
 
     def test_baidu_search(self):
         driver = self.driver
         print u"========【case_0001】 fuck search long time============="
         driver.get(self.base_url + "/")
-        # driver.find_element_by_id("kw").clear()
-        # driver.find_element_by_id("kw").send_keys(u"林志玲")
-        # driver.find_element_by_id("su").click()
+        driver.find_element_by_id("kw").clear()
+        driver.find_element_by_id("kw").send_keys(u"林志玲")
+        driver.find_element_by_id("su").click()
         now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
         # 必须要打印路径HTMLTestRunner才能捕获并且生成路径，\image\**.png 是获取路径的条件，必须这样的目录
         pic_path = 'C:\\Workspace\\HTMLTestRunner\\result\\image\\'+now+'.png'
         print pic_path
         driver.save_screenshot(pic_path)
-        time.sleep(58)
-    def ddest_baidu2_search(self):
-        driver = self.driver
-        print u"========【case_0001】 fuck search long time============="
-        driver.get(self.base_url + "/")
-        driver.find_element_by_id("kdsew").clear()
-        driver.find_element_by_id("kwsdr").send_keys(u"林志玲")
-        # driver.find_element_by_id("su").click()
-        now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
-        # 必须要打印路径HTMLTestRunner才能捕获并且生成路径，\image\**.png 是获取路径的条件，必须这样的目录
-        pic_path = 'C:\\Workspace\\HTMLTestRunner\\result\\image\\'+now+'.png'
-        print pic_path
-        print "hello"
-        driver.save_screenshot(pic_path)
-        time.sleep(2)
+        time.sleep(1)
            
 
     def tearDown(self):
@@ -58,7 +46,6 @@ if __name__ == "__main__":
     now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
     testunit = unittest.TestSuite()
     testunit.addTest(Baidu("test_baidu_search"))
-#     testunit.addTest(Baidu("test_baidu2_search"))
     HtmlFile = "C:\\Workspace\\HTMLTestRunner\\result\\HTMLtemplate.html"
     fp = open(HtmlFile, "wb")
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"Who knows why eclipse pass", description=u"search long time")
